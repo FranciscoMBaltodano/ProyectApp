54296e34ec5f838d7592254db657d96d38a79b67
FranciscoMBaltodano <fmejia@guababit.com>
2025-04-11 07:31:13 -0600

    Update HomeScreen greeting and add expo-updates dependency

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Hola!</ThemedText>
+        <ThemedText type="title">Welcome Hol!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
